a30e994dc32ee9af93cb450ab620e747a4606912
Nupoor <frogeducational@gmail.com>
2020-06-04 16:04:34 +0530

    Register UI Design Done

--- app/src/main/java/com/techmArogya/aarogyaathome/LoginActivity.kt
+++ app/src/main/java/com/techmArogya/aarogyaathome/LoginActivity.kt
@@ -1,13 +1,22 @@
 package com.techmArogya.aarogyaathome
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.login_activity.*
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_activity)
+        val register = findViewById(R.id.registerBtn) as Button
+        register.setOnClickListener {
+          val reqisterIntent = Intent(this, RegisterActivity::class.java)
+            startActivity(reqisterIntent)
+        }
 
     }
 }
