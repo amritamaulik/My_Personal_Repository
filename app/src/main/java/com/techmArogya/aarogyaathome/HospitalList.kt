4240ddb0c10446b7f41d74169432fa9d15aad40e
AC00564941 <AC00564941@TechMahindra.com>
2020-06-05 14:18:11 +0530

    Commit1-amita

---
+++ app/src/main/java/com/techmArogya/aarogyaathome/HospitalList.kt
@@ -0,0 +1,11 @@
+package com.techmArogya.aarogyaathome
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HospitalList : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hospital_list)
+    }
+}
